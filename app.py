1fb2c50d7657d4763046a86d00cead4cdf2e1736
ewint <eliavwinton@gmail.com>
2025-02-08 23:23:24 -0500

    Added .env to .gitignore to secure API tokens

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 from espn_api.football import League
 from threading import Lock
 import os
-from dotenv import load_dotenv
+Dfrom dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env
 
